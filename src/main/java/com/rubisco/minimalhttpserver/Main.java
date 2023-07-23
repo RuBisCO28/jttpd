27b116e1d282edbdbd0449a881e245567024e465
RuBiCO28 <54470624+RuBisCO28@users.noreply.github.com>
2023-07-24 00:27:02 +0900

    check body

--- src/main/java/com/rubisco/minimalhttpserver/Main.java
+++ src/main/java/com/rubisco/minimalhttpserver/Main.java
@@ -15,12 +15,27 @@ public class Main {
             ) {
             String line = reader.readLine();
             StringBuilder header = new StringBuilder();
+            int contentLength = 0;
 
             while(line != null && !line.isEmpty()) {
+                if (line.startsWith("Content-Length")) {
+                    contentLength = Integer.parseInt(line.split(":")[1].trim());
+                }
+
                 header.append(line + "\n");
                 line = reader.readLine();
             }
+
+            String body = null;
+
+            if (contentLength > 0) {
+                char[] buffer = new char[contentLength];
+                reader.read(buffer);
+                body = new String(buffer);
+            }
+
             System.out.println(header);
+            System.out.println(body);
         }
         System.out.println("Server is closed.");
     }
